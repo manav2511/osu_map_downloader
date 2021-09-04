80ac51a04b39e374f31847757c117ff1013dedd1
Manav Raghuwanshi <manav2511@users.noreply.github.com>
2021-09-05 02:25:15 +0530

    Update map_dl.py
    Use temporary directories if no output path specified

--- map_dl.py
+++ map_dl.py
@@ -2,8 +2,16 @@ import re
 import argparse
 import requests
 from zipfile import ZipFile
+import tempfile
+from os.path import basename
 
-def download(ids, path):
+def download(ids, path, name):
+    # use temp_dir if no path specified
+    if len(path) == 0:
+        temp_dir = tempfile.TemporaryDirectory()
+        print("Using temporary directory {0}".format(temp_dir.name))
+        path = temp_dir.name + "\\"
+
     dled = []
     for id in ids:
         url = "https://api.chimu.moe/v1/download/{0}?n=1".format(id)
@@ -12,7 +20,6 @@ def download(ids, path):
 
         if r.status_code == 200:
             d = r.headers.get("Content-Disposition")
-            #filename = requests.utils.unquote(re.findall("filename=\"(.+)\"", d)[0])
 
             filename = path + id + ".osz"
 
@@ -23,11 +30,17 @@ def download(ids, path):
 
             print("Downloaded #{}".format(id))
         else:
-            print("Failed to download #{}! It probably does not exist on the mirror or some error ocurred. \
-                    Please manually download the beatmap!".format(id))
-
+            print("Failed to download #{}! It probably does not exist on the mirror or some error ocurred.\n"
+            "Please manually download the beatmap!".format(id))
     
     print("Finished downloading!")
+
+    #Add all files to zip
+    add_to_zip(dled, name)
+
+    # cleanup temp_dir when done
+    try: temp_dir.cleanup() 
+    except: pass
 
     return dled
 
@@ -35,12 +48,12 @@ def add_to_zip(paths, name):
     print("Adding to zip....")
     with ZipFile(name, 'w') as z:
         for f in paths:
-            z.write(f)
+            z.write(f, basename(f))
 
 # Argument parsing
 ap = argparse.ArgumentParser(description='Download beatmaps from a list of links.')
 
-ap.add_argument("-f", "--file", required=True, metavar="example.txt",
+ap.add_argument("-f", "--file", required=True, metavar="pool.txt",
    help="a text file containing beatmap links seperated by newline")
 ap.add_argument("-n", "--name", required=True, metavar="example.zip",
    help="the name of the zip file to be created")
@@ -57,9 +70,6 @@ with open(file) as f:
 ids = re.findall("(?<=beatmapsets\/)(.*)(?=#)", links)
 
 #Start the download
-dled = download(ids, args["out"])
-
-#Add all files to zip
-add_to_zip(dled, args["name"])
+dled = download(ids, args["out"], args["name"])
 
 print("Done!")
