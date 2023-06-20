3aab4474f819955db4b524b6a80e24a43d829ff9
Manav Raghuwanshi <manav2511@gmail.com>
2023-06-20 18:23:33 +0530

    Add async request code
    Fix regex strings

--- map_dl.py
+++ map_dl.py
@@ -4,120 +4,204 @@ from zipfile import ZipFile
 import tempfile
 from os.path import basename
 from pathlib import Path
+import asyncio
+import aiohttp
+import aiofile
+import time
 
 import requests
-from tqdm import tqdm
+from tqdm.asyncio import tqdm
 
-def getIdsFromLinks(links):
-    ra = "(?<=beatmapsets\/)([0-9]*)(?=#|\n)" # matches format /beatmapsets/xxxxx#xxxxx or /beatmapsets/xxxxx
-    rb = "(.*\/b\/.*)" # matches format /b/xxxxx
+start_time = time.time()
+
+async def get_status_code(url, session, sema):
+    try:
+        async with sema:
+            resp = await session.head(url, allow_redirects=True, timeout=10)
+            async with resp:
+                status_code = resp.status
+                url = resp.url
+
+                return (status_code, str(url))
+    # Meh
+    except:
+        return (-1, url)
+
+async def fetch_file(url, path, filename, session, sema):
+    async with sema:
+        async with session.get(url) as resp:
+            data = await resp.read()
+
+    filepath = path.joinpath(filename)
+    async with aiofile.async_open(
+        filepath, "wb"
+    ) as outfile:
+        await outfile.write(data)
+
+    return filepath
+
+async def getIdsFromLinks(links, session):
+    reg_beatmapset = re.compile(r'(?<=beatmapsets\/)([0-9]*)(?=#|\n|)') # matches format /beatmapsets/xxxxx#xxxxx or /beatmapsets/xxxxx
+    reg_beatmap = re.compile(r'(.*\/b\/.*)') # matches format /b/xxxxx
 
     ids = []
 
-    print("Gettings beatmapset IDs from links..............")
+    print("Gettings beatmapset IDs...")
 
-    for i in re.findall(ra, links):
+    for i in re.findall(reg_beatmapset, links):
+        print(re.findall(reg_beatmapset, links))
         ids.append(i)
 
-    for url in re.findall(rb, links):
-        try:
-            r = requests.head(url, allow_redirects=True, timeout=10)
+    reg_beatmap_found = re.findall(reg_beatmap, links)
 
-            ids.append(re.findall(ra, r.url)[0])
-        except:
-            print("{} is not a valid beatmap URL!".format(url))
+    if len(reg_beatmap_found) > 0:
+        for url in tqdm(reg_beatmap_found, unit='beatmaps', total=len(reg_beatmap_found)):
+            try:
+                r = requests.head(url, allow_redirects=True, timeout=10)
+
+                ids.append(re.findall(reg_beatmapset, r.url)[0])
+            except:
+                print("{} is not a valid beatmap URL!".format(url))
     
     if len(ids) == 0:
         prefix = 'https://osu.ppy.sh/b/'
         beatmap_ids = links.split('\n')
-        for beatmap_id in beatmap_ids:
+        for beatmap_id in tqdm(beatmap_ids, unit='beatmaps', total=len(beatmap_ids)):
             url = prefix + beatmap_id
             try:
                 r = requests.head(url, allow_redirects=True, timeout=10)
-                ids.append(re.findall(ra, r.url)[0])
-            except:
+                ids.append(re.findall(reg_beatmapset, r.url)[0])
+            except Exception as e:
+                print(e)
                 print("{} is not a valid beatmap URL!".format(url))
+
+    # Async code has some seemingly random issues right now
+    # sema = asyncio.BoundedSemaphore(5)
+    # tasks = [get_status_code(url, session, sema) for url in reg_beatmap_found]
+    # responses = await tqdm.gather(*tasks, unit='beatmaps', total=len(tasks))
+    # for resp in responses:
+    #     if resp[0] == 200:
+    #         ids.append(re.findall(reg_beatmapset, resp[1])[0])
+    #     else:
+    #         print("{} is not a valid beatmap URL!".format(resp[1]))
+    
+    # if len(ids) == 0:
+    #     prefix = 'https://osu.ppy.sh/b/'
+    #     beatmap_ids = links.split('\n')
+
+    #     print([prefix + id for id in beatmap_ids])
+
+    #     tasks = [get_status_code(prefix + id, session, sema) for id in beatmap_ids]
+    #     responses = await tqdm.gather(*tasks, unit='beatmaps', total=len(tasks))
+    #     for resp in responses:
+    #         if resp[0] == 200:
+    #             ids.append(re.findall(reg_beatmapset, resp[1])[0])
+    #         else:
+    #             print("{} is not a valid beatmap URL!".format(resp[1]))
 
     return ids
 
-def download(ids, path, name):
-    # use temp_dir if no path specified
-    if len(path) == 0:
-        temp_dir = tempfile.TemporaryDirectory()
-        print("Using temporary directory {}".format(temp_dir.name))
-        path = Path(temp_dir.name)
-    else:
-        path = Path(path)
-        
-        if not path.exists(): raise Exception("The specified path {} does not exist!".format(path)) 
+async def download(ids, path, session):
+    sema = asyncio.BoundedSemaphore(4)
 
-    mirrors = {
-        "chimu.moe": "https://api.chimu.moe/v1/download/{}?n=0",
-        "beatconnect.io": "https://beatconnect.io/b/{}"
-        }
+    mirrors = [{
+        "name": "chimu.moe",
+        "url": "https://api.chimu.moe/v1/download/{}?n=0"
+    },
+    {
+        "name": "beatconnect.io",
+        "url": "https://beatconnect.io/b/{}"
+    }]
+
+    print("Downloading {} maps".format(len(ids)))
 
     # tip for download getting stuck
-    print("\nPress Ctrl + C if download gets stuck for too long.")
+    print("Press Ctrl + C if the download gets stuck")
 
-    dled = []
-    for id in ids:
-        success = False
+    # create file list with name and url
+    files = [{
+        "name": id + ".osz", 
+        "url": mirrors[0]["url"].format(id)
+    } for id in ids]
 
-        # iterate through all available mirrors and try to download the beatmap
-        for m in mirrors:
-            url = mirrors[m].format(id)
-            print("\nTrying to download #{0} from {1}".format(id, m))
+    # tqdm.asyncio.tqdm.as_completed()
+    # tqdm.gather(*flist)
 
-            headers = {'User-Agent': 'Mozilla/5.0'}
+    # dled = {}
 
-            # download the beatmap file
-            name = id + ".osz"
-            filename = path.joinpath(name)
+    # print(files[0]["url"], path, files[0]["name"], sema)
 
-            resp = requests.get(url, stream=True, headers=headers)
-            if resp.status_code == 200:
-                total = int(resp.headers.get('content-length', 0))
+    # download the beatmap files
+    tasks = [fetch_file(file["url"], path, file["name"], session, sema) for file in files]
+    downloaded = await tqdm.gather(*tasks, unit='beatmaps', total=len(tasks))
 
-                with open(filename, 'wb') as file, tqdm(
-                    desc=name,
-                    total=total,
-                    unit='iB',
-                    unit_scale=True,
-                    unit_divisor=1024,
-                ) as bar:
-                    for data in resp.iter_content(chunk_size=1024):
-                        size = file.write(data)
-                        bar.update(size)
+    # for id in ids:
+    #     success = False
 
-                dled.append(filename)
+    #     # iterate through all available mirrors and try to download the beatmap
+    #     for m in mirrors:
+            
+    #         print("\nTrying to download #{0} from {1}".format(id, m))
 
-                if filename.exists():
-                    print("Downloaded #{}".format(id))
-                    success = True
+            
 
-                break
+    #         # download the beatmap file
+    #         name = id + ".osz"
+    #         filename = path.joinpath(name)
+
+    #         resp = requests.get(url, stream=True, headers=headers)
+    #         if resp.status_code == 200:
+    #             total = int(resp.headers.get('content-length', 0))
+
+    #             with open(filename, 'wb') as file, tqdm(
+    #                 desc=name,
+    #                 total=total,
+    #                 unit='iB',
+    #                 unit_scale=True,
+    #                 unit_divisor=1024,
+    #             ) as bar:
+    #                 for data in resp.iter_content(chunk_size=1024):
+    #                     size = file.write(data)
+    #                     bar.update(size)
+
+    #             dled.append(filename)
+
+    #             if filename.exists():
+    #                 print("Downloaded #{}".format(id))
+    #                 success = True
+
+    #             break
         
-        # print fail message if none of the mirrors work or if download didn't complete
-        if not success:
-            print("Failed to download #{}! It probably does not exist on the mirrors.\n"
-            "Please manually download the beatmap from osu.ppy.sh!".format(id))
+    #     # print fail message if none of the mirrors work or if download didn't complete
+    #     if not success:
+    #         print("Failed to download #{}! It probably does not exist on the mirrors.\n"
+    #         "Please manually download the beatmap from osu.ppy.sh!".format(id))
     
-    print("\nFinished downloading!")
+    print("Finished downloading!")
 
     #Add all files to zip
-    add_to_zip(dled, name)
+    #add_to_zip(dled, name)
 
     # cleanup temp_dir when done
-    try: temp_dir.cleanup() 
-    except: pass
+    # try: temp_dir.cleanup() 
+    # except: pass
 
-    return dled
+    return downloaded
+
+    
 
 def add_to_zip(paths, name):
     print("Adding to zip....")
-    with ZipFile(name, 'w') as z:
+
+    with ZipFile(name, 'w') as z, tqdm(
+        desc=name,
+        total=len(paths),
+        unit='files',
+        unit_scale=True
+        ) as bar:
         for f in paths:
             z.write(f, basename(f))
+            bar.update()
 
 # Argument parsing
 ap = argparse.ArgumentParser(description='Download beatmaps from a list of links.')
@@ -131,15 +215,35 @@ ap.add_argument("-o", "--out", required=False, metavar="D:\match_pool\\", defaul
    "use this if you don't want the beatmaps to be deleted after zipping (make sure the folder exists)")
 args = vars(ap.parse_args())
 
-# Read ids from file
-file = args["file"]
 
-with open(file) as f:
-    links = f.read()
+async def main():
+    # Read ids from file
+    file = args["file"]
+    with open(file) as f:
+        links = f.read()
 
-ids = getIdsFromLinks(links)
+    # use temp_dir if no path specified
+    if len(args["out"]) == 0:
+        temp_dir = tempfile.TemporaryDirectory()
+        print("Using temporary directory {}".format(temp_dir.name))
+        path = Path(temp_dir.name)
+    else:
+        path = Path(path)
+        
+        if not path.exists(): raise Exception("The specified path {} does not exist!".format(path))
 
-#Start the download
-dled = download(ids, args["out"], args["name"])
+    session = aiohttp.ClientSession()
 
-print("Done!")
+    ids = await getIdsFromLinks(links, session)
+
+    # Start the download
+    downloaded = await download(ids, path, session)
+
+    # Add maps to zip
+    add_to_zip(downloaded, args["name"])
+
+    await session.close()
+    print("Done!")
+
+asyncio.run(main())
+print("--- %s seconds ---" % (time.time() - start_time))
